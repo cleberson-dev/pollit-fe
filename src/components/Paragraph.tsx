3b539444031f3dc3dd7f8fc513cc2fa397487e21
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2022-03-26 20:18:14 -0300

    Create Paragraph component

---
+++ src/components/Paragraph.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, TextProps } from "react-native";
+
+export const Paragraph = ({ style, ...props }: TextProps): JSX.Element => {
+  return (
+    <Text
+      style={style ? { ...styles.text, ...(style as object) } : styles.text}
+      {...props}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: "white",
+    fontFamily: "MontserratRegular",
+  },
+});

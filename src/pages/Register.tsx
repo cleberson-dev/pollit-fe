693a456b5485062b412c2d279eb745fc700e101a
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2022-03-26 20:37:15 -0300

    Style input and label on Register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,9 +4,11 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  Image,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { Header } from "../components/Header";
+import { Paragraph } from "../components/Paragraph";
 
 export const RegisterPage = (): JSX.Element => {
   return (
@@ -18,35 +20,87 @@ export const RegisterPage = (): JSX.Element => {
     >
       <Header />
 
-      <Text>Registre-se!</Text>
-      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </Text>
+      <View style={styles.hero}>
+        <View>
+          <Paragraph style={styles.descriptionTitle}>Registre-se!</Paragraph>
+          <Paragraph style={styles.descriptionText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </Paragraph>
+        </View>
+        <Image
+          source={require("../../assets/images/register-illustration.png")}
+        />
+      </View>
 
-      <Text>Preencha os campos</Text>
+      <View style={styles.card}>
+        <Text style={styles.formTitle}>Preencha os campos</Text>
 
-      <Text>Nome de usuário</Text>
-      <TextInput />
+        <Text style={styles.inputLabel}>Nome de usuário</Text>
+        <TextInput style={styles.inputField} />
 
-      <Text>Senha</Text>
-      <TextInput />
+        <Text style={styles.inputLabel}>Senha</Text>
+        <TextInput style={styles.inputField} />
 
-      <Text>Redigite sua senha</Text>
-      <TextInput />
+        <Text style={styles.inputLabel}>Redigite sua senha</Text>
+        <TextInput style={styles.inputField} />
 
-      <TouchableOpacity>
-        <Text>Criar conta</Text>
-      </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Criar conta</Text>
+        </TouchableOpacity>
 
-      <Text>
-        Já é membro? <Text>Faça Login</Text>
-      </Text>
+        <Text>
+          Já é membro? <Text>Faça Login</Text>
+        </Text>
+      </View>
     </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 16,
     paddingTop: 32,
     flex: 1,
+  },
+  hero: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+    marginVertical: 20,
+    paddingHorizontal: 16,
+  },
+  descriptionTitle: {
+    fontFamily: "MontserratBold",
+    fontSize: 28,
+    marginBottom: 4,
+  },
+  descriptionText: {
+    flex: 0,
+    width: 200,
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  formTitle: {
+    fontFamily: "MontserratMedium",
+    letterSpacing: 2,
+    textTransform: "uppercase",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  inputLabel: {
+    fontFamily: "MontserratBold",
+    marginBottom: 8,
+  },
+  inputField: {
+    backgroundColor: "#F3F3F4",
+    borderRadius: 5,
+    padding: 4,
+    marginBottom: 20,
   },
 });

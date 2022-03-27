76a4326cefc4053c6dd942543754bea474c827da
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2022-03-27 12:20:00 -0300

    Move gradient background up to App component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,21 @@
 import { useFonts } from "expo-font";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
 import { RegisterPage } from "./src/pages/Register";
+
+const GradientBackground: React.FC = ({ children }) => {
+  return (
+    <LinearGradient
+      colors={["#667EEA", "#764BA2"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.background}
+    >
+      {children}
+    </LinearGradient>
+  );
+};
 
 export default function App() {
   const [loaded] = useFonts({
@@ -12,5 +28,18 @@ export default function App() {
 
   if (!loaded) return null;
 
-  return <RegisterPage />;
+  return (
+    <GradientBackground>
+      <RegisterPage />
+
+      <StatusBar style="light" />
+    </GradientBackground>
+  );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    paddingTop: 48,
+    flex: 1,
+  },
+});
